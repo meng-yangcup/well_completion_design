271a9b7c509100ad1d0863856fd962ebafd7092a
meng-yangcup <78412393+meng-yangcup@users.noreply.github.com>
2021-11-24 19:48:53 +0800

    Add files via upload

--- well_completion.m
+++ well_completion.m
@@ -1,26 +1,27 @@
+%ahead
 %参数设置井径，套管内径，套管外径,井深
 D_h=444.5*10^-3;
 d=313.6*10^-3;
 D=339.7*10^-3;
 h=260;
-%水泥用量计算K1:井径扩大系数；rubber_stopper；pocket；
+
+%水泥用量计算K1:井径扩大系数；RS=rubber_stopper；pocket；
 %
 K1=1.1;
-RS= 4;
+RS = 4;
 pocket=0;
 V = (pi/4)*(K1*h*(D_h^2-D^2)+d^2*RS+D^2*pocket);
 %huo水泥
 rou_drycement=0.26*2.62+0.15*0.7+3.16*(1-0.15-0.26);
-roucm = rou_drycement;
+roudry = rou_drycement;
 rou_drillingfluid = 1.18;
 roudf=rou_drillingfluid;
 %
 K2=1.05;
-roucm=1.82;rouw=1; m=0.515;Q=1.2;
-q=roucm*rouw/(rouw+m*roucm);
+roucm=1.82;rouw=1; m=0.515;
+q=roudry*rouw/(rouw+m*roudry);
 W_c = K2* V *q;
 Vw= m*W_c/rouw;
-
 
 %水利参数测算
 %排量计算
@@ -29,78 +30,112 @@ n=3.32*log10(fai_600-fai_300);
 K=0.511*fai_300/(511^n);
 miu_p = fai_600-fai_300;
 tao_0 = 0.511*(fai_300-miu_p);
+miudf_p = 4.6;
 
 
-%计算分为3类
-%雷诺数过渡流临界雷诺数；m/s;L/s
-Re_c = 2100;
-v_ci=(Re_c*800^n/(8000*roucm*d^n))^(1/(2-n));
-v_ca=(0.2625*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
-Q_ci=pi/40*d^2*v_ci;
-Q_ca=pi/40*(D_h^2-D^2)*v_ca;
-%塞流临界流速临界流速
-Re_cp = 100;
-v_ci_piston = (Re_cp*800^n*K/(8000*roucm*d^n))^(1/(2-n));
-v_ca_piston = (0.0125*800^n*K/(roucm/(D_h^2-D^2)))^(1/(2-n));
-Q_ca_piston = pi/40*(D_h^2-D^2)*v_ca_piston;
+%%%选泵总类数量
+c=[120,150,19.9,33.1;
+   130,150,23.4,28.2;
+   140,150,27.1,24.3;
+   150,150,31.1,21.2;
+   160,150,35.4,18.6;
+   170,150,40.0,16.5];
+c1=zeros(3,4);
+for i=1:3
+    k = input('泵车0123456Enter a number: ');
+    if k==0
+        continue
+    else
+        for j=i:3
+            c1(j,:)=c(k,:);
+        end
+    end
+end
+p_pump = (sum(c1(:,4)));%MPa
+Q_pump = (sum(c1(:,3)))/1000*60;%m^3/min
+V_m = (pi/4)*d^2*h;
+
+T_1 = V/(Q_pump);
+T_2 = 3;
+T_3 = V_m/Q_pump;
+T_4 = 3;
+T = T_1+ T_2+ T_3+ T_4;
+
+%计算分为2类紊流、断塞流
+%%计算雷诺数：
+%管内流动
+vi = Q_pump/60/(pi/4*d^2); %m/s
+Re_i = 8000*roudf*d^n *vi^(2-n)/(800^n*K);
+
+%环空流
+va = Q_pump*60/(pi/4*(D_h^2-D^2));
+Re_a = 8000*roucm*(D_h-D)^n *va^(2-n)/(800^n*K);
+if Re_a > 2100
+    %雷诺数过渡流临界雷诺数；m/s;L/s
+    fprintf('环空流为紊流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+    Q_ci=pi/40*d^2*v_ci;
+    Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+elseif Re_a < 100
+    fprintf('环空流为塞流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+    Q_ci=pi/40*d^2*v_ci;
+    Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+    
+else
+    fprintf('水利参数不合理，请重新选泵');
+end
+
 %套管鞋地层破裂压力rouc:钻井液密度h_cm水泥返高;h_套管井深，pf=MPa;
 h_cm=0;
-pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);
+%pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);rouf:破裂密度
+rouf=2.0;
+pf=rouf* 9.81* (h-h_cm);
 %计算施工最高泵压
 p_hi=10^-3*9.81*(roudf*(h-RS)+roucm*RS);
 p_ha=10^-3*9.81*(roudf*(h-h_cm)+roucm*h_cm);
 %净液柱压差
 delta_p = p_ha - p_hi;
+if Re_a > 2100
 
-x = input('宾汉1Enter a number: ');
-
-if x
-    fi=0.03164/Re_c;
-else
-    a = (log10(n)+2.5)/50;
-    b = (1.4-log10(n))/7;
-    fi = a/Re^b;
-end
+    x = input('宾汉1else幂律Enter a number: ');
+    if x
+        fi=0.03164/Re_c;
+    else
+        a = (log10(n)+2.5)/50;
+        b = (1.4-log10(n))/7;
+        fi = a/Re^b;
+    end
 
 %管内阻力
-p_fi = 2*h*roudf*v_ci^2*fi/d;
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
 %管外阻力
-p_fa = 2*h*roudf*v_ca^2*fi/(D_h-D);
-
-%summarise
-p_max = p_ha +p_hi +p_fi +p_fa;
-
-
-%%%注水泥时间
-y = input('泵车123456Enter a number: ');
-
-switch y
-    case 1
-        Q_pump = 19.9;
-        p_rated = 33.1；
-    case 2
-        Q_pump = 23.4;
-        p_rated = 28.2;
-    case 3
-        Q_pump = 27.1;
-        p_rated = 24.3;
-    case 4
-        Q_pump = 31.1;
-        p_rated = 21.2;
-    case 5
-        Q_pump = 35.4;
-        p_rated = 18.6;
-    case 6 
-        Q_pump = 40;
-        p_rated = 16.5;       
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
+else
+    fi = 16/Re_i;
+    fa = 24/Re_a;
+    %%宾汉和幂律在环空流动计算很复杂；这么简化可能不对
+%管内阻力
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
+%管外阻力
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
 end
-z = input('泵车数量Enter a number: ');
-V_m = (pi/4)*d^2*h;
-T_1 = V/(Q_pump*z);
-T_2 = 3;
-T_3 = V_m/Q_pump;
-T_4 = 3;
-T = T_1+ T_2+ T_3+ T_4;
+%summarise
+p_max = delta_p +p_fi +p_fa;
+if p_pump >p_max
+    fprintf('泵车压力选择合理');
+else
+    fprintf('泵车压力选择不合理');
+end
+
+if p_max < pf
+    fprintf('压力参数合理');
+else
+    fprintf('压力参数不合理，重选泵组');
+end
+
 fprintf('一开水泥返高:%f',h);
 fprintf('一开水泥体积：%f',V);
 fprintf('一开水泥浆密度:%f',roucm);
@@ -110,25 +145,31 @@ fprintf('一开石英砂用量kg：%f',W_c*1000/0.59*0.26);
 fprintf('一开微珠用量kg：%f',W_c*1000/0.59*0.15);
 fprintf('水灰比：%f',m);
 %fprintf('水利参数设计结果：/n');
-%fprintf('套缸直径mm：%f',);
-%fprintf('额定冲次/min：%f',);
-%fprintf('额定排量L/s：%f',);
-
-
-
+for i = 1:3
+    fprintf('套缸直径mm：%f',c1(i:1));
+    fprintf('额定冲次/min：%f',c1(i,2));
+    fprintf('额定排量L/s：%f',c1(i,3));
+    fprintf('额定泵压MPa：%f',c1(i,4));
+end
+fprintf('注水泥施工时间：%f',T);
+fprintf('替钻井液体积：%f',V_m);
+fprintf('排量：%f',Q_pump);
+fprintf('管内反速：%f',vi);
+fprintf('环空反速：%f',va);
 
 %%%%%
 %二开%
 %二开%
 %%%%%
 
-%参数设置井径，套管内径，套管外径,井深
+
+%参数设置井径，套管内径，套管外径,井深，重新赋值
 D_h=311.1*10^-3;
 d=224.4*10^-3;
 D=244.5*10^-3;
 xx = input('班号Enter a number: ');
 yy = input('学号后两位1234Enter a number: ');
-h = 1500+(xx-1)*yy*3;
+h = 1500+(xx-1)*50+yy*3;
 
 %水泥用量计算K1:井径扩大系数；rubber_stopper；pocket；
 %
@@ -138,12 +179,12 @@ pocket=0;
 V = (pi/4)*(K1*h*(D_h^2-D^2)+d^2*RS+D^2*pocket);
 %huo水泥
 rou_drycement=0.26*2.62+0.15*0.7+3.16*(1-0.15-0.26);
-roucm = rou_drycement;
+roudry = rou_drycement;
 rou_drillingfluid = 1.18;
 roudf=rou_drillingfluid;
 %
 K2=1.05;
-roucm=1.82;rouw=1; m=0.515;Q=1.2;
+roucm=1.82;rouw=1; m=0.515;
 q=roucm*rouw/(rouw+m*roucm);
 W_c = K2* V *q;
 Vw= m*W_c/rouw;
@@ -151,83 +192,110 @@ Vw= m*W_c/rouw;
 
 %水利参数测算
 %排量计算
-fai_600=56;fai_300=40;
-n=3.32*log10(fai_600-fai_300);
-K=0.511*fai_300/(511^n);
-miu_p = fai_600-fai_300;
-tao_0 = 0.511*(fai_300-miu_p);
+%fai_600=56;fai_300=40;
+%n=3.32*log10(fai_600-fai_300);
+%K=0.511*fai_300/(511^n);
+%miu_p = fai_600-fai_300;
+%tao_0 = 0.511*(fai_300-miu_p);
 
+%%%选泵总类数量
+c1=zeros(3,4);
+for i=1:3
+    k = input('泵车0123456Enter a number: ');
+    if k==0
+        continue
+    else
+        for j=i:3
+            c1(j,:)=c(k,:);
+        end
+    end
+end
+p_pump = (sum(c1(:,4)));%MPa
+Q_pump = (sum(c1(:,3)))/1000*60;%m^3/min
+V_m = (pi/4)*d^2*h;
 
-%计算分为3类
-%雷诺数过渡流临界雷诺数；m/s;L/s
-Re_c = 2100;
-v_ci=(Re_c*800^n/(8000*roucm*d^n))^(1/(2-n));
-v_ca=(0.2625*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
-Q_ci=pi/40*d^2*v_ci;
-Q_ca=pi/40*(D_h^2-D^2)*v_ca;
-%塞流临界流速临界流速
-Re_cp = 100;
-v_ci_piston = (Re_cp*800^n*K/(8000*roucm*d^n))^(1/(2-n));
-v_ca_piston = (0.0125*800^n*K/(roucm/(D_h^2-D^2)))^(1/(2-n));
-Q_ca_piston = pi/40*(D_h^2-D^2)*v_ca_piston;
+T_1 = V/(Q_pump);
+T_2 = 3;
+T_3 = V_m/Q_pump;
+T_4 = 3;
+T = T_1+ T_2+ T_3+ T_4;
+
+%计算分为2类紊流、断塞流
+%%计算雷诺数：
+%管内流动
+vi = Q_pump/60/(pi/4*d^2); %m/s
+Re_i = 8000*roudf*d^n *vi^(2-n)/(800^n*K);
+
+%环空流
+va = Q_pump*60/(pi/4*(D_h^2-D^2));
+Re_a = 8000*roucm*(D_h-D)^n *va^(2-n)/(800^n*K);
+if Re_a > 2100
+    %雷诺数过渡流临界雷诺数；m/s;L/s
+    fprintf('环空流为紊流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+    Q_ci=pi/40*d^2*v_ci;
+    Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+elseif Re_a < 100
+    fprintf('环空流为塞流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+    Q_ci=pi/40*d^2*v_ci;
+    Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+    
+else
+    fprintf('水利参数不合理，请重新选泵');
+end
+
 %套管鞋地层破裂压力rouc:钻井液密度h_cm水泥返高;h_套管井深，pf=MPa;
 h_cm=0;
-pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);
+%pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);rouf:破裂密度
+rouf=2.0;
+pf=rouf* 9.81* (h-h_cm);
 %计算施工最高泵压
 p_hi=10^-3*9.81*(roudf*(h-RS)+roucm*RS);
 p_ha=10^-3*9.81*(roudf*(h-h_cm)+roucm*h_cm);
 %净液柱压差
 delta_p = p_ha - p_hi;
+if Re_a > 2100
 
-x = input('宾汉1Enter a number: ');
-
-if x
-    fi=0.03164/Re_c;
-else
-    a = (log10(n)+2.5)/50;
-    b = (1.4-log10(n))/7;
-    fi = a/Re^b;
-end
+    x = input('宾汉1else幂律Enter a number: ');
+    if x
+        fi=0.03164/Re_c;
+    else
+        a = (log10(n)+2.5)/50;
+        b = (1.4-log10(n))/7;
+        fi = a/Re^b;
+    end
 
 %管内阻力
-p_fi = 2*h*roudf*v_ci^2*fi/d;
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
 %管外阻力
-p_fa = 2*h*roudf*v_ca^2*fi/(D_h-D);
-
-%summarise
-p_max = p_ha +p_hi +p_fi +p_fa;
-
-
-%%%注水泥时间
-y = input('泵车1234Enter a number: ');
-
-switch y
-    case 1
-        Q_pump = 19.9;
-        p_rated = 33.1;
-    case 2
-        Q_pump = 23.4;
-        p_rated = 28.2;
-    case 3
-        Q_pump = 27.1;
-        p_rated = 24.3;
-    case 4
-        Q_pump = 31.1;
-        p_rated = 21.2;
-    case 5
-        Q_pump = 35.4;
-        p_rated = 18.6;
-    case 6 
-        Q_pump = 40;
-        p_rated = 16.5;       
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
+else
+    fi = 16/Re_i;
+    fa = 24/Re_a;
+    %%宾汉和幂律在环空流动计算很复杂；这么简化可能不对
+%管内阻力
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
+%管外阻力
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
 end
-z = input('泵车数量Enter a number: ');
-V_m = (pi/4)*d^2*h;
-T_1 = V/(Q_pump*z);
-T_2 = 3;
-T_3 = V_m/Q_pump;
-T_4 = 3;
-T = T_1+ T_2+ T_3+ T_4;
+%summarise
+p_max = delta_p +p_fi +p_fa;
+
+if p_pump >p_max
+    fprintf('泵车压力选择合理');
+else
+    fprintf('泵车压力选择不合理');
+end
+
+if p_max < pf
+    fprintf('压力参数合理');
+else
+    fprintf('压力参数不合理，重选泵组');
+end
+
 fprintf('二开水泥返高:%f',h);
 fprintf('二开水泥体积：%f',V);
 fprintf('二开水泥浆密度:%f',roucm);
@@ -237,7 +305,18 @@ fprintf('二开石英砂用量kg：%f',W_c*1000/0.59*0.26);
 fprintf('二开微珠用量kg：%f',W_c*1000/0.59*0.15);
 fprintf('水灰比：%f',m);
 
-
+%fprintf('水利参数设计结果：/n');
+for i = 1:3
+    fprintf('套缸直径mm：%f',c1(i:1));
+    fprintf('额定冲次/min：%f',c1(i,2));
+    fprintf('额定排量L/s：%f',c1(i,3));
+    fprintf('额定泵压MPa：%f',c1(i,4));
+end
+fprintf('注水泥施工时间：%f',T);
+fprintf('替钻井液体积：%f',V_m);
+fprintf('排量：%f',Q_pump);
+fprintf('管内反速：%f',vi);
+fprintf('环空反速：%f',va);
 
 %%%%%
 %三开%
@@ -258,7 +337,6 @@ pocket=4;
 V = (pi/4)*(K1*h*(D_h^2-D^2)+d^2*RS+D^2*pocket);
 %huo水泥
 rou_drycement=0.26*2.62+0.15*0.7+3.16*(1-0.15-0.26);
-roucm = rou_drycement;
 rou_drillingfluid = 1.18;
 roudf=rou_drillingfluid;
 %
@@ -268,86 +346,104 @@ q=roucm*rouw/(rouw+m*roucm);
 W_c = K2* V *q;
 Vw= m*W_c/rouw;
 
+%%%选泵总类数量
+c1=zeros(3,4);
+for i=1:3
+    k = input('泵车0123456Enter a number: ');
+    if k==0
+        continue
+    else
+        for j=i:3
+            c1(j,:)=c(k,:);
+        end
+    end
+end
+p_pump = (sum(c1(:,4)));%MPa
+Q_pump = (sum(c1(:,3)))/1000*60;%m^3/min
+V_m = (pi/4)*d^2*h;
 
-%水利参数测算
-%排量计算
-fai_600=56;fai_300=40;
-n=3.32*log10(fai_600-fai_300);
-K=0.511*fai_300/(511^n);
-miu_p = fai_600-fai_300;
-tao_0 = 0.511*(fai_300-miu_p);
+T_1 = V/(Q_pump);
+T_2 = 3;
+T_3 = V_m/Q_pump;
+T_4 = 3;
+T = T_1+ T_2+ T_3+ T_4;
 
+%计算分为2类紊流、断塞流
+%%计算雷诺数：
+%管内流动
+vi = Q_pump/60/(pi/4*d^2); %m/s
+Re_i = 8000*roudf*d^n *vi^(2-n)/(800^n*K);
 
-%计算分为3类
-%雷诺数过渡流临界雷诺数；m/s;L/s
-Re_c = 2100;
-v_ci=(Re_c*800^n/(8000*roucm*d^n))^(1/(2-n));
-v_ca=(0.2625*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
-Q_ci=pi/40*d^2*v_ci;
-Q_ca=pi/40*(D_h^2-D^2)*v_ca;
-%塞流临界流速临界流速
-Re_cp = 100;
-v_ci_piston = (Re_cp*800^n*K/(8000*roucm*d^n))^(1/(2-n));
-v_ca_piston = (0.0125*800^n*K/(roucm/(D_h^2-D^2)))^(1/(2-n));
-Q_ca_piston = pi/40*(D_h^2-D^2)*v_ca_piston;
+%环空流
+va = Q_pump*60/(pi/4*(D_h^2-D^2));
+Re_a = 8000*roucm*(D_h-D)^n *va^(2-n)/(800^n*K);
+if Re_a > 2100
+    %雷诺数过渡流临界雷诺数；m/s;L/s
+    fprintf('环空流为紊流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+ %   Q_ci=pi/40*d^2*v_ci;
+ %   Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+elseif Re_a < 100
+    fprintf('环空流为塞流');
+    v_ci=(Re_i*800^n/(8000*roucm*d^n))^(1/(2-n));
+    v_ca=(Re_a*800^n*K/(roucm/(D_h-D)^n))^(1/(2-n));
+%    Q_ci=pi/40*d^2*v_ci;
+%    Q_ca=pi/40*(D_h^2-D^2)*v_ca;
+    
+else
+    fprintf('水利参数不合理，请重新选泵');
+end
+
 %套管鞋地层破裂压力rouc:钻井液密度h_cm水泥返高;h_套管井深，pf=MPa;
 h_cm=0;
-pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);
+%pf=10^-3*9.81*(h*roucm+ (h-h_cm)*roudf);rouf:破裂密度
+rouf=2.0;
+pf=rouf* 9.81* (h-h_cm);
 %计算施工最高泵压
 p_hi=10^-3*9.81*(roudf*(h-RS)+roucm*RS);
 p_ha=10^-3*9.81*(roudf*(h-h_cm)+roucm*h_cm);
 %净液柱压差
 delta_p = p_ha - p_hi;
+if Re_a > 2100
 
-x = input('宾汉1Enter a number: ');
-
-if x
-    fi=0.03164/Re_c;
-else
-    a = (log10(n)+2.5)/50;
-    b = (1.4-log10(n))/7;
-    fi = a/Re^b;
-end
+    x = input('宾汉1else幂律Enter a number: ');
+    if x
+        fi=0.03164/Re_c;
+    else
+        a = (log10(n)+2.5)/50;
+        b = (1.4-log10(n))/7;
+        fi = a/Re^b;
+    end
 
 %管内阻力
-p_fi = 2*h*roudf*v_ci^2*fi/d;
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
 %管外阻力
-p_fa = 2*h*roudf*v_ca^2*fi/(D_h-D);
-
-%summarise
-p_max = p_ha +p_hi +p_fi +p_fa;
-
-
-%%%注水泥时间
-y = input('泵车1234Enter a number: ');
-
-switch y
-    case 1
-        Q_pump = 19.9;
-        p_rated = 33.1;
-    case 2
-        Q_pump = 23.4;
-        p_rated = 28.2;
-    case 3
-        Q_pump = 27.1;
-        p_rated = 24.3;
-    case 4
-        Q_pump = 31.1;
-        p_rated = 21.2;
-    case 5
-        Q_pump = 35.4;
-        p_rated = 18.6;
-    case 6 
-        Q_pump = 40;
-        p_rated = 16.5;       
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
+else
+    fi = 16/Re_i;
+    fa = 24/Re_a;
+    %%宾汉和幂律在环空流动计算很复杂；这么简化可能不对
+%管内阻力
+    p_fi = 2*h*roudf*1000*v_ci^2*fi/d;
+%管外阻力
+    p_fa = 2*h*roucm*1000*v_ca^2*fi/(D_h-D);
 end
-z = input('泵车数量Enter a number: ');
-V_m = (pi/4)*d^2*h;
-T_1 = V/(Q_pump*z);
-T_2 = 3;
-T_3 = V_m/Q_pump;
-T_4 = 3;
-T = T_1+ T_2+ T_3+ T_4;
+%summarise
+p_max = delta_p +p_fi +p_fa;
+
+if p_pump >p_max
+    fprintf('泵车压力选择合理');
+else
+    fprintf('泵车压力选择不合理');
+end
+
+if p_max < pf
+    fprintf('压力参数合理');
+else
+    fprintf('压力参数不合理，重选泵组');
+end
+
 fprintf('三开水泥返高:%f',h);
 fprintf('三开水泥体积：%f',V);
 fprintf('三开水泥浆密度:%f',roucm);
@@ -356,8 +452,18 @@ fprintf('三开清水用量m^3:%f',Vw);
 fprintf('三开石英砂用量kg：%f',W_c*1000/0.59*0.26);
 fprintf('三开微珠用量kg：%f',W_c*1000/0.59*0.15);
 fprintf('水灰比：%f',m);
-
-
+%fprintf('水利参数设计结果：/n');
+for i = 1:3
+    fprintf('套缸直径mm：%f',c1(i:1));
+    fprintf('额定冲次/min：%f',c1(i,2));
+    fprintf('额定排量L/s：%f',c1(i,3));
+    fprintf('额定泵压MPa：%f',c1(i,4));
+end
+fprintf('注水泥施工时间：%f',T);
+fprintf('替钻井液体积：%f',V_m);
+fprintf('排量：%f',Q_pump);
+fprintf('管内反速：%f',vi);
+fprintf('环空反速：%f',va);
 
 
 
